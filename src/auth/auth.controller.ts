0fab1d1e5b00535625f3ca0d9ec7c9458e804cbc
NabilRaiyan <rraiyan77@gmail.com>
2024-10-09 16:21:10 +0600

    creating validation pipes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request } from 'express';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp(@Req() req: Request) {
-    console.log(req.body);
+  signUp(@Body() dto: AuthDto) {
+    console.log({ dto });
     return this.authService.signUp();
   }
 
