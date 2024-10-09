5d25559d134af51c75e4f8d3e836b02fcd4f38a6
NabilRaiyan <rraiyan77@gmail.com>
2024-10-10 02:18:31 +0600

    creating unique email for user and handling exceptions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
-
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -11,15 +11,26 @@ export class AuthService {
 
   async signUp(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-        firstName: dto.firstName,
-        lastName: dto.lastName,
-      },
-    });
-    return user;
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken');
+        }
+      }
+
+      throw error;
+    }
   }
 
   logIn() {}
