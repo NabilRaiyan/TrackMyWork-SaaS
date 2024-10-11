acd8b3e8e799321ca02a1f63b95207a2b8ac9ef4
NabilRaiyan <rraiyan77@gmail.com>
2024-10-11 17:43:35 +0600

    creating jwt token and authinticate the user using jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './strategy';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })], // loading dotenv file var in our project
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '30m' },
+    }),
+  ], // loading dotenv file var in our project
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,27 @@
 // Until 1 hr 38 min
 // Link: https://www.youtube.com/watch?v=GHTA143_b-s
 
-
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto, AuthSignInDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 
 // User sign up
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signUp(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
@@ -25,8 +34,7 @@ export class AuthService {
           lastName: dto.lastName,
         },
       });
-      delete user.hash;
-      return user;
+      return this.singInToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -55,7 +63,21 @@ export class AuthService {
       throw new ForbiddenException('Incorrect Credentials');
     }
 
-    delete user.hash;
-    return user;
+    return this.singInToken(user.id, user.email);
+  }
+
+  // JWT token
+  async singInToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    if (!userId) throw new UnauthorizedException();
+    if (!email) throw new UnauthorizedException();
+
+    const payload = { sub: userId, email: email };
+
+    return {
+      access_token: await this.jwt.signAsync(payload),
+    };
   }
 }
